4e752ec362e45364bc6ed2775a2e9cffdb49528c
tommy-DEV1 <114732399+tommy-DEV1@users.noreply.github.com>
2023-06-28 17:09:13 +0530

    Delete parser.h

--- parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "scanner.h" /* struct tokenStruct */
-#include "source.h"  /* struct sourceStruct */
-
-struct nodeStruct *parseSimpleCommand(struct tokenStruct *tok);
-
-#endif
